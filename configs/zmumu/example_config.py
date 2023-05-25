5fdb918840bd6ac69d61e479e1ac59ce6c825d57
Davide Valsecchi <davide.valsecchi@cern.ch>
2023-05-25 18:37:32 +0200

    Added config for image

--- configs/zmumu/example_config.py
+++ configs/zmumu/example_config.py
@@ -99,6 +99,7 @@ run_options = {
         "env"            : "singularity",
         "workers"        : 1,
         "scaleout"       : 50,
+        "worker_image"   : "/cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/cms-analysis/general/pocketcoffea:lxplus-cc7-latest",
         "queue"          : "microcentury",
         "walltime"       : "00:40:00",
         "mem_per_worker" : "4GB", # GB
