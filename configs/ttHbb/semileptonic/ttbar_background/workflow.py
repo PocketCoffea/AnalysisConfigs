716fa1d66538ce40f4c92d207422efa5737b93c6
mmarchegiani <matmarcheg@gmail.com>
2023-10-04 17:52:10 +0200

    Compute mbb

--- configs/ttHbb/semileptonic/ttbar_background/workflow.py
+++ configs/ttHbb/semileptonic/ttbar_background/workflow.py
@@ -1,6 +1,6 @@
 import awkward as ak
 from pocket_coffea.workflows.tthbb_base_processor import ttHbbBaseProcessor
-from pocket_coffea.lib.deltaR_matching import metric_eta, metric_phi, metric_mass
+from pocket_coffea.lib.deltaR_matching import metric_eta, metric_phi
 
 class ttbarBackgroundProcessor(ttHbbBaseProcessor):
 
@@ -12,18 +12,25 @@ class ttbarBackgroundProcessor(ttHbbBaseProcessor):
         deltaR = ak.flatten(self.events["BJetGood"].metric_table(self.events["BJetGood"]), axis=2)
         deltaEta = ak.flatten(self.events["BJetGood"].metric_table(self.events["BJetGood"], metric=metric_eta), axis=2)
         deltaPhi = ak.flatten(self.events["BJetGood"].metric_table(self.events["BJetGood"], metric=metric_phi), axis=2)
-        mbb = ak.flatten(self.events["BJetGood"].metric_table(self.events["BJetGood"], metric=metric_mass), axis=2)
-        mbb = mbb[deltaR > 0.]
         deltaR = deltaR[deltaR > 0.]
         deltaEta = deltaEta[deltaEta > 0.]
         deltaPhi = deltaPhi[deltaPhi > 0.]
 
-        idx_closest = ak.argmin(deltaR, axis=1)
+        # Get the deltaR with no possibility of repetition of identical b-jet pairs
 
-        # Compute the minimum deltaR(b, b) in the event
+        # Get all the possible combinations of b-jet pairs
+        pairs = ak.argcombinations(self.events["BJetGood"], 2, axis=1)
+        b1 = self.events.BJetGood[pairs.slot0]
+        b2 = self.events.BJetGood[pairs.slot1]
+
+        # Compute deltaR between the pairs
+        deltaR_unique = b1.delta_r(b2)
+        idx_pairs_sorted = ak.argsort(deltaR_unique, axis=1)
+        pairs_sorted = pairs[idx_pairs_sorted]
+
+        # Compute the minimum deltaR(b, b), deltaEta(b, b), deltaPhi(b, b) and the invariant mass of the closest b-jet pair
         self.events["deltaRbb_min"] = ak.min(deltaR, axis=1)
         self.events["deltaEtabb_min"] = ak.min(deltaEta, axis=1)
         self.events["deltaPhibb_min"] = ak.min(deltaPhi, axis=1)
-        self.events["mbb"] = mbb[idx_closest]
+        self.events["mbb"] = (self.events.BJetGood[pairs_sorted.slot0] + self.events.BJetGood[pairs_sorted.slot1]).mass
 
-        breakpoint()
