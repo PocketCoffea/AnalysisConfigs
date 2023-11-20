435d8ca5151a7be59069cce7a1076fe57f302f42
mmarchegiani <matmarcheg@gmail.com>
2023-11-20 18:25:07 +0100

    Include parton matching in workflow

--- configs/ttHbb/semileptonic/ttbar_background/workflow.py
+++ configs/ttHbb/semileptonic/ttbar_background/workflow.py
@@ -1,8 +1,14 @@
 import awkward as ak
 from pocket_coffea.workflows.tthbb_base_processor import ttHbbBaseProcessor
 from pocket_coffea.lib.deltaR_matching import metric_eta, metric_phi
+from pocket_coffea.lib.deltaR_matching import object_matching
+from pocket_coffea.lib.parton_provenance import get_partons_provenance_ttHbb, get_partons_provenance_ttbb4F, get_partons_provenance_tt5F
 
 class ttbarBackgroundProcessor(ttHbbBaseProcessor):
+    def __init__(self, cfg) -> None:
+        super().__init__(cfg=cfg)
+        self.dr_min = self.workflow_options["parton_jet_min_dR"]
+
     def apply_object_preselection(self, variation):
         super().apply_object_preselection(variation=variation)
         if self._isMC:
@@ -51,6 +57,72 @@ class ttbarBackgroundProcessor(ttHbbBaseProcessor):
         self.events["deltaPhibb_min"] = ak.min(deltaPhi, axis=1)
         self.events["mbb"] = (self.events.BJetGood[pairs_sorted.slot0] + self.events.BJetGood[pairs_sorted.slot1]).mass
 
+    def do_parton_matching(self) -> ak.Array:
+        # Selects quarks at LHE level
+        isOutgoing = self.events.LHEPart.status == 1
+        isParton = (abs(self.events.LHEPart.pdgId) < 6) | (self.events.LHEPart.pdgId == 21)
+        quarks = self.events.LHEPart[isOutgoing & isParton]
+
+        # Get the interpretation
+        if self._sample == "TTbbSemiLeptonic":
+            prov = get_partons_provenance_ttbb4F(
+                ak.Array(quarks.pdgId, behavior={}), ak.ArrayBuilder()
+            ).snapshot()
+        elif self._sample == "TTToSemiLeptonic":
+            prov = get_partons_provenance_tt5F(
+                ak.Array(quarks.pdgId, behavior={}), ak.ArrayBuilder()
+            ).snapshot()
+        else:
+            prov = -1 * ak.ones_like(quarks)
+
+        # Adding the provenance to the quark object
+        quarks = ak.with_field(quarks, prov, "provenance")
+
+        # Calling our general object_matching function.
+        # The output is an awkward array with the shape of the second argument and None where there is no matching.
+        # So, calling like this, we will get out an array of matched_quarks with the dimension of the JetGood.
+        matched_quarks, matched_genjets, deltaR_matched = object_matching(
+            quarks, self.events.GenJetGood, dr_min=self.dr_min
+        )
+
+        # Saving leptons and neutrino parton level
+        self.events["LeptonParton"] = self.events.LHEPart[
+            (self.events.LHEPart.status == 1)
+            & (abs(self.events.LHEPart.pdgId) > 10)
+            & (abs(self.events.LHEPart.pdgId) < 15)
+        ]
+
+        self.events["Parton"] = quarks
+        self.events["PartonMatched"] = ak.with_field(
+            matched_quarks, deltaR_matched, "dRMatchedGenJet"
+        )
+        self.events["GenJetGoodMatched"] = ak.with_field(
+            matched_genjets, deltaR_matched, "dRMatchedGenJet"
+        )
+        self.matched_partons_mask = ~ak.is_none(self.events.GenJetGoodMatched, axis=1)
+
+    def count_partons(self):
+        self.events["nParton"] = ak.num(self.events.Parton, axis=1)
+        self.events["nPartonMatched"] = ak.count(
+            self.events.PartonMatched.pt, axis=1
+        )  # use count since we have None
+
+    def count_additional_jets(self):
+        is_additional_bjet = ak.fill_none(
+            (self.events.GenJetGoodMatched.hadronFlavour == 5) &
+            ( (self.events.PartonMatched.provenance == 1) | (self.events.PartonMatched.provenance == 4) ),
+            False
+        )
+        is_additional_cjet = ak.fill_none(
+            (self.events.GenJetGoodMatched.hadronFlavour == 4) &
+            ( (self.events.PartonMatched.provenance == 1) | (self.events.PartonMatched.provenance == 4) ),
+            False
+        )
+        self.events["BGenJetGoodExtra"] = self.events.GenJetGoodMatched[is_additional_bjet]
+        self.events["CGenJetGoodExtra"] = self.events.GenJetGoodMatched[is_additional_cjet]
+        self.events["nBGenJetGoodExtra"] = ak.num(self.events.BGenJetGoodExtra)
+        self.events["nCGenJetGoodExtra"] = ak.num(self.events.CGenJetGoodExtra)
+
     def count_objects(self, variation):
         super().count_objects(variation=variation)
         if self._isMC:
@@ -58,3 +130,9 @@ class ttbarBackgroundProcessor(ttHbbBaseProcessor):
             self.events["nBGenJetGood"] = ak.num(self.events.BGenJetGood)
             self.events["nCGenJetGood"] = ak.num(self.events.CGenJetGood)
             self.events["nLGenJetGood"] = ak.num(self.events.LGenJetGood)
+
+    def process_extra_after_presel(self, variation) -> ak.Array:
+        if self._isMC:
+            self.do_parton_matching()
+            self.count_partons()
+            self.count_additional_jets()
