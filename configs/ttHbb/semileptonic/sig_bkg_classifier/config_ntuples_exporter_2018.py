b5b3021611a7d85a17f2e9df73e44b0509425b1a
mmarchegiani <matmarcheg@gmail.com>
2024-02-28 15:49:29 +0100

    Update config files

---
+++ configs/ttHbb/semileptonic/sig_bkg_classifier/config_ntuples_exporter_2018.py
@@ -0,0 +1,148 @@
+from pocket_coffea.utils.configurator import Configurator
+from pocket_coffea.lib.cut_definition import Cut
+from pocket_coffea.lib.columns_manager import ColOut
+from pocket_coffea.lib.cut_functions import get_nObj_min, get_HLTsel, get_nBtagMin
+from pocket_coffea.parameters.cuts import passthrough
+from pocket_coffea.parameters.histograms import *
+
+import workflow
+from workflow import ttbarBackgroundProcessor
+
+import custom_cut_functions
+import custom_cuts
+from custom_cut_functions import *
+from custom_cuts import *
+import os
+localdir = os.path.dirname(os.path.abspath(__file__))
+
+# Loading default parameters
+from pocket_coffea.parameters import defaults
+default_parameters = defaults.get_default_parameters()
+defaults.register_configuration_dir("config_dir", localdir+"/params")
+
+parameters = defaults.merge_parameters_from_files(default_parameters,
+                                                  f"{localdir}/params/object_preselection_semileptonic.yaml",
+                                                  f"{localdir}/params/triggers.yaml",
+                                                  f"{localdir}/params/lepton_scale_factors.yaml",
+                                                  f"{localdir}/params/plotting_style.yaml",
+                                                  update=True)
+
+cfg = Configurator(
+    parameters = parameters,
+    datasets = {
+        "jsons": [f"{localdir}/datasets/signal_ttHTobb_local.json",
+                  f"{localdir}/datasets/signal_ttHTobb_ttToSemiLep_local.json",
+                  f"{localdir}/datasets/backgrounds_MC_TTbb_local.json",
+                  f"{localdir}/datasets/backgrounds_MC_ttbar_local.json"
+                  ],
+        "filter" : {
+            "samples": [#"ttHTobb",
+                        "ttHTobb_ttToSemiLep",
+                        #"TTbbSemiLeptonic",
+                        #"TTToSemiLeptonic"
+                        ],
+            "samples_exclude" : [],
+            "year": ["2018"]
+        },
+        "subsamples": {
+            'TTbbSemiLeptonic' : {
+                'TTbbSemiLeptonic_tt+LF'   : [get_genTtbarId_100_eq(0)],
+                'TTbbSemiLeptonic_tt+C'    : [get_genTtbarId_100_eq([41, 42, 43, 44, 45, 46])],
+                'TTbbSemiLeptonic_tt+B'    : [get_genTtbarId_100_eq([51, 52, 53, 54, 55, 56])],
+            },
+            'TTToSemiLeptonic' : {
+                'TTToSemiLeptonic_tt+LF'   : [get_genTtbarId_100_eq(0)],
+                'TTToSemiLeptonic_tt+C'    : [get_genTtbarId_100_eq([41, 42, 43, 44, 45, 46])],
+                'TTToSemiLeptonic_tt+B'    : [get_genTtbarId_100_eq([51, 52, 53, 54, 55, 56])],
+            },
+        }
+    },
+
+    workflow = ttbarBackgroundProcessor,
+    workflow_options = {"parton_jet_min_dR": 0.3},
+    
+    skim = [get_nObj_min(4, 15., "Jet"),
+            get_nBtagMin(3, 15., coll="Jet"),
+            get_HLTsel(primaryDatasets=["SingleEle", "SingleMuon"])],
+    
+    preselections = [semileptonic_presel],
+    categories = {
+        "baseline": [passthrough],
+        "semilep_LHE": [semilep_lhe]
+    },
+
+    weights= {
+        "common": {
+            "inclusive": [
+                "genWeight", "lumi","XS",
+                "pileup",
+                "sf_ele_reco", "sf_ele_id",
+                "sf_mu_id", "sf_mu_iso",
+                "sf_btag",
+                "sf_jet_puId",
+            ],
+            "bycategory": {},
+        },
+        "bysample": {},
+    },
+    variations = {
+        "weights": {"common": {"inclusive": [], "bycategory": {}}, "bysample": {}},
+    },
+    
+    variables = {},
+    columns = {
+        "common": {
+            "inclusive": [],
+            "bycategory": {
+                    "semilep_LHE": [
+                        ColOut(
+                            "Parton",
+                            ["pt", "eta", "phi", "mass", "pdgId", "provenance"]
+                        ),
+                        ColOut(
+                            "PartonMatched",
+                            ["pt", "eta", "phi","mass", "pdgId", "provenance", "dRMatchedJet"],
+                        ),
+                        ColOut(
+                            "JetGood",
+                            ["pt", "eta", "phi", "hadronFlavour", "btagDeepFlavB"],
+                        ),
+                        ColOut(
+                            "JetGoodMatched",
+                            ["pt", "eta", "phi", "hadronFlavour", "btagDeepFlavB", "dRMatchedJet"],
+                        ),
+                        ColOut("LeptonGood",
+                               ["pt","eta","phi", "pdgId", "charge", "mvaTTH"],
+                               pos_end=1, store_size=False),
+                        ColOut("MET", ["phi","pt","significance"]),
+                        ColOut("Generator",["x1","x2","id1","id2","xpdf1","xpdf2"]),
+                        ColOut("LeptonParton",["pt","eta","phi","mass","pdgId"]),
+                    ]
+            }
+        },
+        "bysample": {
+            #"ttHTobb": {
+            #    "bycategory": {
+            #        "semilep_LHE": [
+            #            ColOut("HiggsParton",
+            #                   ["pt","eta","phi","mass","pdgId"], pos_end=1, store_size=False),
+            #        ]
+            #    }
+            #},
+            "ttHTobb_ttToSemiLep": {
+                "bycategory": {
+                    "semilep_LHE": [
+                        ColOut("HiggsParton",
+                               ["pt","eta","phi","mass","pdgId"], pos_end=1, store_size=False),
+                    ]
+                }
+            }
+        },
+    },
+)
+
+# Registering custom functions
+import cloudpickle
+cloudpickle.register_pickle_by_value(workflow)
+cloudpickle.register_pickle_by_value(custom_cut_functions)
+cloudpickle.register_pickle_by_value(custom_cuts)
