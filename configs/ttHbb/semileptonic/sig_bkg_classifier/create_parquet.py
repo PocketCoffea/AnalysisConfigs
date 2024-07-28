73f60fc5be484419886f8e5101cfa388f089b82d
mmarchegiani <matmarcheg@gmail.com>
2024-07-28 18:44:46 +0200

    Generalize script

--- configs/ttHbb/semileptonic/sig_bkg_classifier/create_parquet.py
+++ configs/ttHbb/semileptonic/sig_bkg_classifier/create_parquet.py
@@ -1,7 +1,12 @@
 import os
+import argparse
 import awkward as ak
 
-basedir = "/work/mmarcheg/ttHbb/EFT/sig_bkg_classifier/sig_bkg_ntuples_ttHTobb_ttToSemiLep_improved_matching/ntuples/output_columns_parton_matching/parton_matching_20_06_24"
-datasets =os.listdir(basedir)
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", type=str, required=True)
+args = parser.parse_args()
+
+#basedir = "/work/mmarcheg/ttHbb/EFT/sig_bkg_classifier/sig_bkg_ntuples_ttHTobb_ttToSemiLep_improved_matching/ntuples/output_columns_parton_matching/parton_matching_20_06_24"
+datasets =os.listdir(args.input)
 for d in datasets:
-    ak.to_parquet.dataset(os.path.join(basedir, d, "semilep_LHE"))
+    ak.to_parquet.dataset(os.path.join(args.input, d, "semilep"))
