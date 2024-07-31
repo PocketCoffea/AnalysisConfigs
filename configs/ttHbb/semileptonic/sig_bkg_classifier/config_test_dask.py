538830fbecd7d671a20112b55703d90acc5e64f1
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-07-31 16:43:35 +0200

    SPANET on dask test

---
+++ configs/ttHbb/semileptonic/sig_bkg_classifier/config_test_dask.py
@@ -0,0 +1,102 @@
+from pocket_coffea.utils.configurator import Configurator
+from pocket_coffea.lib.cut_definition import Cut
+from pocket_coffea.lib.columns_manager import ColOut
+from pocket_coffea.lib.cut_functions import get_nObj_min, get_HLTsel, get_nBtagMin
+from pocket_coffea.parameters.cuts import passthrough
+from pocket_coffea.parameters.histograms import *
+
+import workflow_test_spanet
+from workflow_test_spanet import TestDaskProcessor
+
+import custom_cut_functions
+import custom_cuts
+from custom_cut_functions import *
+from custom_cuts import *
+import os
+localdir = os.path.dirname(os.path.abspath(__file__))
+
+# Loading default parameters
+from pocket_coffea.parameters import defaults
+default_parameters = defaults.get_default_parameters()
+defaults.register_configuration_dir("config_dir", localdir+"/params")
+
+parameters = defaults.merge_parameters_from_files(default_parameters,
+                                                  f"{localdir}/params/object_preselection_semileptonic.yaml",
+                                                  f"{localdir}/params/triggers.yaml",
+                                                  f"{localdir}/params/lepton_scale_factors.yaml",
+                                                  f"{localdir}/params/plotting_style.yaml",
+                                                  update=True)
+
+cfg = Configurator(
+    parameters = parameters,
+    datasets = {
+        "jsons": [f"{localdir}/datasets/signal_ttHTobb_local.json",
+                  f"{localdir}/datasets/signal_ttHTobb_ttToSemiLep_local.json",
+                  f"{localdir}/datasets/backgrounds_MC_TTbb_local.json",
+                  f"{localdir}/datasets/backgrounds_MC_ttbar_local.json"
+                  ],
+        "filter" : {
+            "samples": ["ttHTobb",
+                        "ttHTobb_ttToSemiLep",
+                        "TTbbSemiLeptonic",
+                        "TTToSemiLeptonic"
+                        ],
+            "samples_exclude" : [],
+            "year": ["2016_PreVFP",
+                     "2016_PostVFP",
+                     "2017",
+                     "2018"
+                     ] #All the years
+        },
+        "subsamples": {
+            'TTbbSemiLeptonic' : {
+                'TTbbSemiLeptonic_tt+LF'   : [get_genTtbarId_100_eq(0)],
+                'TTbbSemiLeptonic_tt+C'    : [get_genTtbarId_100_eq([41, 42, 43, 44, 45, 46])],
+                'TTbbSemiLeptonic_tt+B'    : [get_genTtbarId_100_eq([51, 52, 53, 54, 55, 56])],
+            },
+            'TTToSemiLeptonic' : {
+                'TTToSemiLeptonic_tt+LF'   : [get_genTtbarId_100_eq(0)],
+                'TTToSemiLeptonic_tt+C'    : [get_genTtbarId_100_eq([41, 42, 43, 44, 45, 46])],
+                'TTToSemiLeptonic_tt+B'    : [get_genTtbarId_100_eq([51, 52, 53, 54, 55, 56])],
+            },
+        }
+    },
+
+    workflow = TestDaskProcessor,
+    
+    skim = [get_nObj_min(4, 15., "Jet"),
+            get_nBtagMin(3, 15., coll="Jet", wp="M"),
+            get_HLTsel(primaryDatasets=["SingleEle", "SingleMuon"])],
+    
+    preselections = [semileptonic_presel],
+    categories = {
+        "baseline": [passthrough],
+        "semilep_LHE": [semilep_lhe]
+    },
+
+    weights= {
+        "common": {
+            "inclusive": [
+                "genWeight", "lumi","XS",
+                "pileup",
+                "sf_ele_reco", "sf_ele_id",
+                "sf_mu_id", "sf_mu_iso",
+                "sf_btag",
+                "sf_jet_puId",
+            ],
+            "bycategory": {},
+        },
+        "bysample": {},
+    },
+    variations = {
+        "weights": {"common": {"inclusive": [], "bycategory": {}}, "bysample": {}},
+    },
+    
+    variables = {},
+)
+
+# Registering custom functions
+import cloudpickle
+cloudpickle.register_pickle_by_value(workflow_test_spanet)
+cloudpickle.register_pickle_by_value(custom_cut_functions)
+cloudpickle.register_pickle_by_value(custom_cuts)
---
+++ configs/ttHbb/semileptonic/sig_bkg_classifier/onnx_executor.py
@@ -0,0 +1,43 @@
+from pocket_coffea.executors.executors_T3_CH_PSI import DaskExecutorFactory
+from dask.distributed import WorkerPlugin, Worker, Client
+
+
+class WorkerInferenceSessionPlugin(WorkerPlugin):    
+    async def setup(self, worker: Worker):
+        import onnxruntime as ort
+
+        sess_options = ort.SessionOptions()
+
+        sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+        
+        session = ort.InferenceSession(
+    f"/work/dvalsecc/ttHbb/AnalysisConfigs/configs/ttHbb/semileptonic/sig_bkg_classifier/spanet.onnx", 
+            sess_options = sess_options,
+            providers=['CPUExecutionProvider']
+        )
+
+        worker.data["model_session"] = session
+
+        
+# Create an instance of the plugin
+inference_session_plugin = WorkerInferenceSessionPlugin()
+
+class OnnxExecutorFactory(DaskExecutorFactory):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def setup(self):
+        super().setup()
+        # now setting up and registering the ONNX plugin
+        inference_session_plugin = WorkerInferenceSessionPlugin()
+        # Registering the session plugin
+        self.dask_client.register_worker_plugin(inference_session_plugin)
+
+
+
+
+
+
+def get_executor_factory(executor_name, **kwargs):
+    return OnnxExecutorFactory(**kwargs)
