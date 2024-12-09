c0f672000c025037723c7779f8667ebe3dd1fb2e
Matteo Marchegiani <matmarcheg@gmail.com>
2024-12-09 15:13:50 +0100

    Include all shape systematics and data in datacard

--- configs/ttHbb/semileptonic/dctr/make_datacards.py
+++ configs/ttHbb/semileptonic/dctr/make_datacards.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from coffea.util import load
 from pocket_coffea.utils.datacard import Datacard
@@ -7,31 +8,65 @@ from pocket_coffea.utils.systematics import SystematicUncertainty
 parser = argparse.ArgumentParser(description="Make datacards")
 parser.add_argument("-i", "--input", help="Coffea input file with histograms", required=True)
 parser.add_argument("-o", "--output", help="Output directory for datacards", default="datacards", required=False)
-parser.add_argument("--histogram", help="Histogram to make datacard for", default="spanet_tthbb_transformed",  required=False)
+parser.add_argument("--year", help="Year of the datacards", required=True)
 args = parser.parse_args()
 
 df = load(args.input)
-histograms = df["variables"][args.histogram]
 datasets_metadata = df["datasets_metadata"]
 
+samples_data = [s for s in datasets_metadata["by_datataking_period"][args.year].keys() if s.startswith("DATA")]
+
 processes = [
+    Process(name="tthbb", samples=["ttHTobb"], is_signal=True),
     Process(name="ttlf", samples=["TTToSemiLeptonic__TTToSemiLeptonic_tt+LF"], is_signal=False),
     Process(name="ttcc", samples=["TTToSemiLeptonic__TTToSemiLeptonic_tt+C"], is_signal=False),
     Process(name="ttbb", samples=["TTbbSemiLeptonic__TTbbSemiLeptonic_tt+B"], is_signal=False),
+    Process(name="tt_dilepton", samples=["TTTo2L2Nu"], is_signal=False),
     Process(name="singletop", samples=["SingleTop"], is_signal=False),
-    Process(name="signal", samples=["ttHTobb"], is_signal=True),
+    Process(name="vjets", samples=["WJetsToLNu_HT", "DYJetsToLL"], is_signal=False),
+    Process(name="ttv", samples=["TTV"], is_signal=False),
+    Process(name="diboson", samples=["VV"], is_signal=False),
 ]
+
+data_processes = [Process(name="data_obs", samples=samples_data, is_signal=False, is_data=True)]
 
 systematics = [
-    SystematicUncertainty(name="lumi", typ="lnN", processes=["ttlf", "ttcc", "ttbb", "signal"], value=0.9),
+    SystematicUncertainty(name="lumi", typ="lnN", processes=[p.name for p in processes if p not in samples_data], value=1.025),
 ]
+shape_systematics = [
+    "pileup",
+    "sf_ele_reco", "sf_ele_id",
+    "sf_ele_trigger_era", "sf_ele_trigger_ht",
+    "sf_ele_trigger_pileup", "sf_ele_trigger_stat",
+    "sf_mu_id", "sf_mu_iso", "sf_mu_trigger",
+    "sf_btag_cferr1", "sf_btag_cferr2",
+    "sf_btag_hf", "sf_btag_hfstats1", "sf_btag_hfstats2",
+    "sf_btag_lf", "sf_btag_lfstats1", "sf_btag_lfstats2",
+    "sf_jet_puId", #"sf_top_pt",
+    #"sf_btag_calib", "sf_ttlf_calib",
+]
+for syst in shape_systematics:
+    systematics.append(SystematicUncertainty(name=syst, typ="shape", processes=[p.name for p in processes if p not in samples_data], value=1.0))
 
-datacard = Datacard(
-    histograms=histograms,
-    datasets_metadata=datasets_metadata,
-    processes=processes,
-    systematics=systematics,
-    year="2018",
-    category="CR",
-)
-datacard.dump(directory=args.output, card_name="datacard.txt", shapes_filename="shapes.root")
+print("Systematic lnN uncertainties: ", [s.name for s in systematics if s.typ == "lnN"])
+print("Systematic shape uncertainties: ", [s.name for s in systematics if s.typ == "shape"])
+
+histograms_dict = {
+    "CR" : df["variables"]["dctr_index"],
+    "CR_ttlf" : df["variables"]["jets_Ht"],
+    "SR" : df["variables"]["spanet_tthbb_transformed"],
+}
+
+for cat, histograms in histograms_dict.items():
+    print(f"Creating datacard for category: {cat}")
+    datacard = Datacard(
+        histograms=histograms,
+        datasets_metadata=datasets_metadata,
+        processes=processes,
+        data_processes=data_processes,
+        systematics=systematics,
+        year=args.year,
+        category=cat,
+    )
+    datacard.dump(directory=args.output, card_name=f"datacard_{cat}.txt", shapes_filename=f"shapes_{cat}.root")
+    print(f"Datacard saved in {os.path.abspath(os.path.join(args.output, f'datacard_{cat}.txt'))}")
