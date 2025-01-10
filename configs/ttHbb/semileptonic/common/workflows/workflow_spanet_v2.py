fd0f247e42a22fd5dfc6cfc1b938a4daa6298e0c
Matteo Marchegiani <matmarcheg@gmail.com>
2025-01-10 09:45:32 +0100

    Include quantile transformer

--- configs/ttHbb/semileptonic/common/workflows/workflow_spanet_v2.py
+++ configs/ttHbb/semileptonic/common/workflows/workflow_spanet_v2.py
@@ -98,8 +98,8 @@ class SpanetInferenceProcessor(ttHbbPartonMatchingProcessor):
         )
 
         # Transform ttHbb score with quantile transformation
-        #params_quantile_transformer = self.params["quantile_transformer"][self.events.metadata["year"]]
-        #transformer = WeightedQuantileTransformer(n_quantiles=params_quantile_transformer["n_quantiles"], output_distribution=params_quantile_transformer["output_distribution"])
-        #transformer.load(params_quantile_transformer["file"])
-        #transformed_score = transformer.transform(self.events.spanet_output.tthbb)
-        #self.events["spanet_output"] = ak.with_field(self.events["spanet_output"], transformed_score, "tthbb_transformed")
+        params_quantile_transformer = self.params["quantile_transformer"][self.events.metadata["year"]]
+        transformer = WeightedQuantileTransformer(n_quantiles=params_quantile_transformer["n_quantiles"], output_distribution=params_quantile_transformer["output_distribution"])
+        transformer.load(params_quantile_transformer["file"])
+        transformed_score = transformer.transform(self.events.spanet_output.tthbb)
+        self.events["spanet_output"] = ak.with_field(self.events["spanet_output"], transformed_score, "tthbb_transformed")
